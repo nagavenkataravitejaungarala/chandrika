53c10f1fad285004bdb20580dcf92e1fa58d9741
nagavenkataravitejaungarala <130119502+nagavenkataravitejaungarala@users.noreply.github.com>
2025-01-06 16:09:53 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heroImage = document.querySelector('.hero-image');
+    heroImage.style.backgroundImage = "url(C:\Users\91798\Desktop\Edusage\E-Learning-570x255.jpg)";  // Replace with your image path
+});
+
+
+
+
+const courses = [
+    { name: "Web Development", image: "web img.webp", className: "webdev-image" },
+    { name: "Cybersecurity", image: "cyber.jpg" },
+    { name: "Python Programming", image: "OIP.jpg" },
+    { name: "Andriod Development", image: "android.jpg" },
+    { name: "Statistics", image: "statistics.jpg" },
+    { name: "SQL Database", image: "sql.jpg" },
+];
